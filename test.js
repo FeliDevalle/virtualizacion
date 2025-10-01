3cba27ce942889340401aae2d6cf36b65dce30ff
thiag <thiagocoronel@hotmail.com.ar>
2025-10-01 00:10:59 -0300

    test regex pattern: PEM

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 // AWS Access Key ID 
 const awsId = "AKIA11111111111111";
 password = 'MiPass123!';
+const fakePem = `-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBFAKEFAKEDATAEXAMPLE...
+-----END RSA PRIVATE KEY-----`;
