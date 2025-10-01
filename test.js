e23963bc9ee8ae318c522c80c5b6cb8824d3bc21
thiag <thiagocoronel@hotmail.com.ar>
2025-10-01 00:31:34 -0300

    test regex pattern: AWS AKIA

--- test.js
+++ test.js
@@ -1,9 +1,2 @@
 // AWS Access Key ID 
 const awsId = "AKIA11111111111111";
-password = 'MiPass123!';
-const fakePem = `-----BEGIN RSA PRIVATE KEY-----
-MIIBOgIBAAJBFAKEFAKEDATAEXAMPLE...
------END RSA PRIVATE KEY-----`;
-
-
-const mongoConn = "mongodb://testuser:TestPass!@mongo.example.com:27017/testdb";
