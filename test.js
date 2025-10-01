e32e3e64f315dcf1ee82e935fa77c92b708013a8
thiag <thiagocoronel@hotmail.com.ar>
2025-09-30 23:56:50 -0300

    test literal pattern: secret

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-"API_KEY = '123456'"
-"API_KEY = '123456'"
 secret = ''
-API_KEY
-secret = 'hola'
-module.exports = {
-    API_KEY: "sk_test_FAKE_1234567890",
-    secret: "esto_es_un_secret_de_prueba2",    
-    password: "MiPass123!"                   
-  };
-
-// AWS Access Key ID 
-const awsId = "AKIA11111111111111";
-
-const fakePem = `-----BEGIN RSA PRIVATE KEY-----
-MIIBOgIBAAJBFAKEFAKEDATAEXAMPLE...
------END RSA PRIVATE KEY-----`;
