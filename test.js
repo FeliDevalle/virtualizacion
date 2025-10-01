1d3b70a6ede07f9d33de27915f8d49bf0de24ea3
thiag <thiagocoronel@hotmail.com.ar>
2025-09-30 23:53:14 -0300

    regex pattern test: PEM

--- test.js
+++ test.js
@@ -4,10 +4,14 @@ secret = ''
 API_KEY
 secret = 'hola'
 module.exports = {
-    API_KEY: "sk_test_FAKE_1234567890",        // literal: API_KEY
-    secret: "esto_es_un_secret_de_prueba2",    // literal: secret
-    password: "MiPass123!"                    // literal: password
+    API_KEY: "sk_test_FAKE_1234567890",
+    secret: "esto_es_un_secret_de_prueba2",    
+    password: "MiPass123!"                   
   };
 
 // AWS Access Key ID 
 const awsId = "AKIA11111111111111";
+
+const fakePem = `-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBFAKEFAKEDATAEXAMPLE...
+-----END RSA PRIVATE KEY-----`;
