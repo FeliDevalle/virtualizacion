1b6fb1deb1184a9d44e178ac7954bc8cd8f59788
thiag <thiagocoronel@hotmail.com.ar>
2025-10-02 00:20:22 -0300

    test regex pattern: PEM

--- APL/powershell/ejercicio4/test.js
+++ APL/powershell/ejercicio4/test.js
@@ -3,3 +3,7 @@ PASSWORD = "1234567890";
 const awsId = "AKIA11111111111111";
 
 const mongoConn = "mongodb://testuser:TestPass!@mongo.example.com:27017/testdb";
+
+const fakePem = `-----BEGIN RSA PRIVATE KEY-----
+MIIBOgIBAAJBFAKEFAKEDATAEXAMPLE...
+-----END RSA PRIVATE KEY-----`;
